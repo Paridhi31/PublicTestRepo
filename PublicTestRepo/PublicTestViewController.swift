9a672ede43d0aa8fa2683a7da40779fe54fdfeb6
Paridhi <paridhi.malviya@thoughtworks.com>
2020-05-13 13:38:42 +0530

    Initial commit

---
+++ PublicTestRepo/PublicTestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PublicTestViewController.swift
+//  PublicTestRepo
+//
+//  Created by Paridhi Malviya on 13/05/20.
+//  Copyright © 2020 Paridhi Malviya. All rights reserved.
+//
+
+import UIKit
+
+class PublicTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PublicTestRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PublicTestRepo
+//
+//  Created by Paridhi Malviya on 13/05/20.
+//  Copyright © 2020 Paridhi Malviya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
